308f7b4d0afd2aa31a5358a60c2f245870fc24bd
Kiran <Kiranpatil73827@gmail.com>
2024-10-10 15:42:47 +0530

    Added Navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="bg-gray-200 h-16 ">
+        <div className="flex items-center border-2  justify-between h-full ">
+          <div>
+            <a
+              className="text-blue-500 text-xl underline"
+              href="https://intranet.cb.amrita.edu/"
+            >
+              Intranet
+            </a>
+          </div>
+          <div>
+            <h2 className="font-mono text-3xl">
+              Amrita Institutional Repository
+            </h2>
+          </div>
+          <div className="text-blue-700">
+            <a href="">Login</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
