fc5e6cd0c873ed2e7340c8ca33f2f0ff95a800d1
Kiran <Kiranpatil73827@gmail.com>
2024-10-10 15:52:03 +0530

    Updated card

--- src/Components/Home/Card.jsx
+++ src/Components/Home/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Card({number}) {
   return (
     <div>
-      <div className="border-2  w-64 shadow-md flex justify-center items-center  rounded-xl  h-36 transition ease-in-out   hover:scale-105 cursor-pointer">
+      <div className="border  w-64 shadow-md flex justify-center items-center  rounded-xl  h-36 transition ease-in-out   hover:scale-105 cursor-pointer">
         <h2 className="text-xl font-bold">Semester-{number}</h2>
       </div>
     </div>
