cac04da10b48e12a0e39eb7bacae30d9a35b2037
Kiran <Kiranpatil73827@gmail.com>
2024-10-12 15:51:10 +0530

    Updated home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -15,7 +15,7 @@ export default function Home() {
       <div>
         <h2 className="text-2xl underline ml-4">Semester Wise Papers</h2>
       </div>
-      <div className="mt-20 h-96">
+      <div className="mt-20 h-96 ">
         <div className="flex flex-wrap gap-20 gap-y-10  px-32 w-full items-center ">
           {renderCards(9)}
         </div>
