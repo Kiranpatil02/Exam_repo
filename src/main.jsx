cf7dab78484cdb2af1f3370bc3f3e8aed9352088
Kiran <Kiranpatil73827@gmail.com>
2024-10-12 21:09:39 +0530

    Updated main

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './Components/Home/Home.jsx'
-import Content from './Components/Content/Content.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Components/Home/Home.jsx";
+import Content from "./Components/Content/Content.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+import config from "./config/config.js";
+import Login from "./Components/Login/Login.jsx";
 
-
-const router=createBrowserRouter([
+const router = createBrowserRouter([
   {
-    path:"/",
-    element:<App/>,
-    children:[
+    path: "/",
+    element: <App />,
+    children: [
       {
-        path:"/",
-        element:<Home/>,
+        path: "/",
+        element: <Home />,
       },
       {
-        path:"/semester-",
-        element:<Content/>
+        path: "/semester-",
+        element: <Content />,
+      },
+      {
+        path:"/sign-in",
+        element:<Login/>
       }
-    ]
-  }
-])
+      
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <ClerkProvider publishableKey={config.clerk_key}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
+  </StrictMode>
+);
