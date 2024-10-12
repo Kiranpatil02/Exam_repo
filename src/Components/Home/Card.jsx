7fd20f4f3d4d7afdb38d5c373ada92b916c46a91
Kiran <Kiranpatil73827@gmail.com>
2024-10-12 15:55:31 +0530

    Updated link

--- src/Components/Home/Card.jsx
+++ src/Components/Home/Card.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Card({number}) {
   return (
+    <Link to={`/semester-`}>
     <div>
       <div className="border  w-64 shadow-md flex justify-center items-center  rounded-xl  h-36 transition ease-in-out   hover:scale-105 cursor-pointer">
         <h2 className="text-xl font-bold">Semester-{number}</h2>
       </div>
     </div>
+    </Link>
   );
 }
