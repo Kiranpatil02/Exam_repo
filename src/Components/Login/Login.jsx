69f2424f7b40cbf597f95ec9b6b08d869517cbd0
Kiran <Kiranpatil73827@gmail.com>
2024-10-13 19:33:36 +0530

    Updated login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { SignIn } from "@clerk/clerk-react";
+import { IoMdArrowBack } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <div className="w-fit mx-auto mt-20 ">
-      <SignIn appearance={{
-        elements:{
-            footerActionLink: "hidden",
-        },
-      }} path="/sign-in" signUpUrl="null" />
-      
-      
-    </div>
+    <>
+      <div className="ml-7 mt-5 w-fit">
+        <span className="text-3xl">
+          <Link to={"/"}>
+            <IoMdArrowBack />
+          </Link>
+        </span>
+      </div>
+      <div className="w-fit mx-auto mt-10 ">
+        <SignIn
+          appearance={{
+            elements: {
+              footerActionLink: "hidden",
+            },
+          }}
+          path="/sign-in"
+          signUpUrl="null"
+        />
+      </div>
+    </>
   );
 }
