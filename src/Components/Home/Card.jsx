cb16f7bb28b043572f39f2be3522e497a25b1d54
Kiran <Kiranpatil73827@gmail.com>
2024-10-19 22:05:00 +0530

    Updated dynamic routing

--- src/Components/Home/Card.jsx
+++ src/Components/Home/Card.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Card({ number }) {
   return (
-    <Link to={`/semester-`}>
+    <Link to={`/semester/${number}`}>
       <div>
         <div className="border  w-64 shadow-sm flex justify-center items-center  rounded-xl  h-36 transition ease-in-out   hover:scale-105 cursor-pointer">
           <h2 className="text-xl font-semibold">Semester-{number}</h2>
--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/semester-",
+        path: "/semester/:id",
         element: <Content />,
       },
       {
