d44ea860551b5a89db5fa8567028650b0e025357
Kiran <Kiranpatil73827@gmail.com>
2024-10-22 11:12:21 +0530

    Downloads files without sas tokens

--- src/Azure/Storage.js
+++ src/Azure/Storage.js
@@ -1,17 +1,18 @@
-import { BlobServiceClient } from "@azure/storage-blob";
+import { BlobServiceClient,StorageSharedKeyCredential } from "@azure/storage-blob";
 const dotenv = await import('dotenv');
 dotenv.config()
 
 const accountname=process.env.AZURE_STORAGE_ACCOUNT_NAME;
-const sasToken=process.env.AZURE_STORAGE_SAS_TOKEN;
+const accountkey=process.env.AZURE_STORAGE_ACCOUNT_KEY;
 if (!accountname) throw Error('Azure Storage accountName not found');
-if (!sasToken) throw Error('Azure Storage accountKey not found');
+if (!accountkey) throw Error('Azure Storage accountKey not found');
+
+const sharedKeyCredential = new StorageSharedKeyCredential(accountname, accountkey);
 
 const blobServiceUri = `https://examfiles.blob.core.windows.net/`
 
 const blobServiceClient = new BlobServiceClient(
-    `${blobServiceUri}?${sasToken}`,
-    null
+    blobServiceUri,sharedKeyCredential
   );
 
 async function download(){
