c53a0bf76b58a8ca8bbf1c11a0afa7cca054d919
Kiran <Kiranpatil73827@gmail.com>
2024-11-03 01:00:11 +0530

    Updated with specific semesters

--- src/Azure/Storage.js
+++ src/Azure/Storage.js
@@ -1,20 +1,21 @@
 import { BlobServiceClient } from "@azure/storage-blob";
 
-
-class Service{
-
-  constructor(){
-    this.accountName="examfiles";
-    this.sas="?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2024-11-01T19:19:00Z&st=2024-11-01T11:19:00Z&spr=https&sig=su8ZYSQw7yrI8UVGHV9N0aGpmYBztsgoW1K5hlthCqY%3D"
-    this.blobServiceClient=new BlobServiceClient(`https://${this.accountName}.blob.core.windows.net${this.sas}`);
-    this.containerClient = this.blobServiceClient.getContainerClient("semester-5");
-    
+class Service {
+  constructor() {
+    this.accountName = "examfiles";
+    this.sas =
+      "?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2024-11-19T00:21:44Z&st=2024-11-02T16:21:44Z&spr=https&sig=5jfPbENCKSdGMpSoHjUy8YEYMw7vCGdwLfa%2Bki9gjXY%3D";
+    this.blobServiceClient = new BlobServiceClient(
+      `https://${this.accountName}.blob.core.windows.net${this.sas}`
+    );
   }
 
-  async listfiles(){
-    try{
+  async listfiles() {
+    try {
       let i = 1;
-      for await (const blob of this.blobServiceClient.findBlobsByTags("probability='2023'")) {
+      for await (const blob of this.blobServiceClient.findBlobsByTags(
+        "probability='2024'"
+      )) {
         console.log(`Blob ${i++}: ${blob.name}`);
       }
 
@@ -26,49 +27,66 @@ class Service{
     }
   }
 
-  async getfile(tag,value){
-    try{
-      const filename=this.blobServiceClient.findBlobsByTags(`${tag}='${value}'`);
-      for await(const i of filename){
-        this.downloadfile(i.name)
+  async getfile(tag, value, examname) {
+    console.log("tag=", tag);
+    console.log("value=", value);
+    console.log("examname=", examname);
+    tag = tag.replace(/\s+/g,' ').trim().split(" ").slice(1).join("_").toUpperCase();
+    console.log(tag);
+    try {
+      const filename = this.blobServiceClient.findBlobsByTags(
+        `"${tag}"='${value}' AND "exam"='${examname}'`
+      );
+      let blobfound = null;
+      for await (const i of filename) {
+        blobfound = i;
+        // this.downloadfile(i.name);
+        return i.name;
       }
-
-    }catch(e){
-      console.log("Failed logging file")
+      if (!blobfound) {
+        console.log("No blobs found");
+        throw new Error();
+      }
+    } catch (e) {
+      console.log("Failed logging file", e);
+      return false;
     }
   }
 
-  async downloadfile(filename){
-    try{
-      const blobClient=this.containerClient.getBlobClient(`${filename}`);
-      const downloadblobresponse=await blobClient.download();
-      const downloaded=await  downloadblobresponse.blobBody
+  async downloadfile(filename,id) {
+    try {
+      this.containerClient =
+        this.blobServiceClient.getContainerClient(`semester-${id}`);
+      const blobClient = this.containerClient.getBlobClient(`${filename}`);
+      const downloadblobresponse = await blobClient.download();
+      const downloaded = await downloadblobresponse.blobBody;
       // console.log("Downloaded blob content", downloaded);
 
-      const url=URL.createObjectURL(await downloaded);
+      const url = URL.createObjectURL(await downloaded);
 
-      const a=document.createElement("a");
-      a.style.display="none";
-      a.href=url;
-      a.download="Quiz_1.pdf";
+      const a = document.createElement("a");
+      a.style.display = "none";
+      a.href = url;
+      a.download = `${filename}`;
       document.body.appendChild(a);
       a.click();
       document.body.removeChild(a);
 
       URL.revokeObjectURL(url);
-
-
-
-    }
-    catch(e){
-      console.log("Failed downloading")
+    } catch (e) {
+      console.log("Failed downloading", e);
     }
   }
-
 }
 
-const serv=new Service()
-export default serv
+const serv = new Service();
+// serv.getfile("22AIE301 PROBABILISTIC REASONING","2024","quiz3").then((e)=>{
+//   if(e){
+//     console.log("sucess")
 
+//   }else{
+//     console.log("Failed")
+//   }
+// })
 
-
+export default serv;
