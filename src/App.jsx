818e45be74fe413c05699054618e957d3ea3aa77
Kiran <Kiranpatil73827@gmail.com>
2024-11-06 17:53:11 +0530

    removed waste code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
-import Home from "./Components/Home/Home";
-import Card from "./Components/Home/Card";
-import Content from "./Components/Content/Content";
 import { Outlet } from "react-router-dom";
 
 export default function App() {
