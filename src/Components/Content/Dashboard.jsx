cf6984cd3ec573601f06e39c0202f3838de5ebbd
Kiran <Kiranpatil73827@gmail.com>
2024-11-07 19:02:35 +0530

    Dashboard init

---
+++ src/Components/Content/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export default function Dashboard(){
+    return(
+        <>
+        <div className='mt-5 max-w-screen-2xl mx-auto flex flex-wrap border-2 justify-center items-center gap-10'>
+           
+        </div>
+        
+        </>
+    )
+}
