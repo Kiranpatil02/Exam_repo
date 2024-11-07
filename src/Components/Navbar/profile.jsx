f177c8f7febc0d39ff7668e208610729a805f5fe
Kiran <Kiranpatil73827@gmail.com>
2024-11-07 19:02:16 +0530

    Users info

---
+++ src/Components/Navbar/profile.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useUser } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
+
+export default function Profile() {
+  const { user } = useUser();
+  return (
+    <>
+    <Link to={'/Dashboard'}>
+      <div class="flex items-center gap-1 hover:cursor-pointer ">
+        <p className="text-gray-700">{user.firstName}</p>
+        <div className="flex items-center justify-center w-9 h-9  rounded-full bg-blue-500 text-white font-bold"></div>
+      </div>
+    </Link>
+    </>
+  );
+}
