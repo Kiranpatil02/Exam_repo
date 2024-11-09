181b50c18ed890bb6f9620e481dafed0314ed38f
Kiran <Kiranpatil73827@gmail.com>
2024-11-09 18:33:35 +0530

    Basic upload layout

---
+++ src/Components/User/Upload.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { FaFileUpload } from "react-icons/fa";
+
+export default function UploadFile() {
+  return (
+    <>
+      <div className="mt-10 mx-auto border w-1/3 rounded-lg">
+        <form
+          className="py-6 px-9 "
+          method="POST"
+        >
+          <div class="mb-5">
+            <label
+              for="email"
+              className="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Send files to this email:
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="example@domain.com"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>
+
+          <div className="mb-6 pt-4">
+            <label className="mb-5 block text-xl font-semibold text-[#07074D]">
+              Upload File
+            </label>
+
+            <div className="mb-8">
+              <input type="file" name="file" id="file"  accept=".pdf" className="sr-only" />
+              <label
+                for="file"
+                className="relative flex min-h-[200px] items-center justify-center rounded-md border border-dashed border-[#e0e0e0] p-12 text-center"
+              >
+                <div>
+                  <span className="mb-2 block text-xl font-semibold text-[#07074D]">
+                    Drop files here
+                  </span>
+                  <span className="mb-2 block text-base font-medium text-[#6B7280]">
+                    Or
+                  </span>
+                  <span className="inline-flex rounded border border-[#e0e0e0] py-2 px-7 text-base font-medium text-[#07074D]">
+                    Browse
+                  </span>
+                </div>
+              </label>
+            </div>
+          </div>
+
+          <div>
+            <button className="hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none">
+              Send File
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
