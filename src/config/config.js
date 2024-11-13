914c38825696a41df428a18061581dbe1583b1fb
Kiran <Kiranpatil73827@gmail.com>
2024-11-13 11:10:05 +0530

    Updated with environment

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,8 @@
 const config={
-    clerk_key:String(import.meta.env.VITE_CLERK_PUBLISHABLE_KEY)
+    clerk_key:String(import.meta.env.VITE_CLERK_PUBLISHABLE_KEY),
+    cosmos_connection:String(import.meta.env.VITE_AZURE_COSMOS_CONNECTIONSTRING),
+    account_key:String(import.meta.env.VITE_STORAGE_ACCOUNT_KEY),
+    sas_token:String(import.meta.env.VITE_AZURE_SAS_TOKEN),
+    account_name:String(import.meta.env.VITE_AZURE_STORAGE_ACCOUNT)
 }
 export default config
