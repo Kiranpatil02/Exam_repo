c816935c0412b3bd13680759e96b050bb0394ff3
Kiran <Kiranpatil73827@gmail.com>
2024-11-13 18:47:10 +0530

    Small fix

--- src/Components/Content/Dashboard.jsx
+++ src/Components/Content/Dashboard.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import DashCard from './dashcard'
-export default function Dashboard(){
-    return(
-        <>
-        <div className='flex flex-wrap gap-20 gap-y-10  justify-center w-full items-center  mt-5'>
-            <DashCard/>
-        </div>
-        
-        </>
-    )
+import React from "react";
+import DashCard from "./dashcard";
+import { Link } from "react-router-dom";
+import { IoArrowBackSharp } from "react-icons/io5";
+export default function Dashboard() {
+  return (
+    <>
+      <div className="text-3xl absolute mt-5 w-fit ml-10 hover:cursor-pointer">
+        <Link to={"/"}>
+          <IoArrowBackSharp />
+        </Link>
+      </div>
+      <div className="flex flex-wrap gap-20 gap-y-10  justify-center w-full items-center  mt-5">
+        <DashCard />
+      </div>
+    </>
+  );
 }
